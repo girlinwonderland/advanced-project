a072ff6ead1881e865cd5e1b3db4ab92e08a4462
girlinwonderland <lisa.kossova@mail.ru>
2023-02-09 20:03:32 +0300

    настройка jest

--- src/app/providers/router/index.tsx
+++ src/app/providers/router/index.tsx
@@ -6,7 +6,11 @@ export const AppRouter = () => (
     <Suspense fallback={<div>...loading</div>}>
         <Routes>
             {Object.values(routeConfig).map((({ path, element }) => (
-                <Route key={path} path={path} element={<div className="page-wrapper">{element}</div>} />
+                <Route
+                    key={path}
+                    path={path}
+                    element={<div className="page-wrapper">{element}</div>}
+                />
             )))}
         </Routes>
     </Suspense>
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,8 +3,8 @@ export type TClassNamesMode = Record<string, boolean | string>
 /** Функция по заданным параметрам возвращает строку классов */
 export const classNames = (
     mainClass: string,
-    mode?: TClassNamesMode,
-    otherClasses?: string[],
+    mode: TClassNamesMode = {},
+    otherClasses: string[] = [],
 ): string => (
     [mainClass, ...otherClasses.filter(Boolean), ...Object.entries(mode)
         .filter(([, value]) => value).map(([value]) => value)]
