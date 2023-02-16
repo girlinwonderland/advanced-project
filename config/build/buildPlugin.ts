37f20e85defab4b42b86fac9b0841e182959bacf
girlinwonderland <lisa.kossova@mail.ru>
2023-02-16 18:56:35 +0300

    плагин анализа поместили в дев

--- config/build/buildPlugin.ts
+++ config/build/buildPlugin.ts
@@ -3,16 +3,23 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-export const buildPlugin = (path: string, isDev: boolean): webpack.WebpackPluginInstance[] => [
-    new HtmlWebpackPlugin({ template: path }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
-];
+export const buildPlugin = (path: string, isDev: boolean): webpack.WebpackPluginInstance[] => {
+    const plugins: webpack.WebpackPluginInstance[] = [
+        new HtmlWebpackPlugin({ template: path }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+    ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+    }
+
+    return plugins;
+};
