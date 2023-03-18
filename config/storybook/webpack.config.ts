1ba6c08492f64072b01d81421e51f0b785bd4df7
girlinwonderland <lisa.kossova@mail.ru>
2023-03-18 17:56:04 +0300

    ts ignore в конфиге

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -5,9 +5,8 @@ import { buildCssLoaders } from '../build/loaders/buildCssLoaders';
 export default ({ config }: {config: webpack.Configuration }) => {
     config.resolve!.modules!.push(path.resolve(__dirname, '..', '..', 'src'));
     config.resolve!.extensions!.push('.ts', '.tsx');
-    // eslint-disable-next-line no-param-reassign
-    // @ts-ignore
-    config.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
+    const rules = config.module! as RuleSetRule[];
+    config.module!.rules = rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
