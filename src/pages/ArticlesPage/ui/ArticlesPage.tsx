ae3b3f8efa0f96472e35726d91a0549ae35cf9fb
girlinwonderland <lisa.kossova@mail.ru>
2023-04-10 17:10:47 +0300

    article list

--- src/pages/ArticlesPage/ui/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage.tsx
@@ -1,21 +1,15 @@
 import { memo, useCallback } from 'react';
-import { useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
 import { useSearchParams } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useInitialEffect } from 'shared/lib/hooks/useInit';
 import { useAppDispatch } from 'shared/lib/hooks/useDispatch';
 import { Page } from 'widgets/Page/Page';
-import { ArticleList } from 'entities/Article';
 import { initArticlesPage } from 'pages/ArticlesPage/model/services/initArticlesPage';
 import { fetchNextArticlesPage } from '../model/services/fetchNextArticlesPage';
-import { ArticleSliceReducer, getArticles } from '../model/slices/articlesPageSlice';
-import {
-    getArticlesPageIsLoading,
-    getArticlesPageView,
-} from '../model/selectors';
+import { ArticleSliceReducer } from '../model/slices/articlesPageSlice';
 import { ArticlesPageFilters } from './ArticlesPageFilters/ArticlesPageFilters';
+import { ArticleInfiniteList } from './ArticleInfiniteList';
 import styles from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
@@ -28,12 +22,9 @@ const reducers: ReducerList = {
 
 export const ArticlesPage = memo((props: ArticlesPageProps) => {
     const { className } = props;
-    const { t } = useTranslation();
 
     const dispatch = useAppDispatch();
-    const articles = useSelector(getArticles.selectAll);
-    const isLoading = useSelector(getArticlesPageIsLoading);
-    const view = useSelector(getArticlesPageView);
+
     const [searchParams] = useSearchParams();
 
     const onLoadNextPart = useCallback(() => {
@@ -51,12 +42,7 @@ export const ArticlesPage = memo((props: ArticlesPageProps) => {
                 className={classNames(styles.ArticlesPage, {}, [className])}
             >
                 <ArticlesPageFilters />
-                <ArticleList
-                    isLoading={isLoading}
-                    view={view}
-                    articles={articles}
-                    className={styles.list}
-                />
+                <ArticleInfiniteList className={styles.list} />
             </Page>
         </DynamicModuleLoader>
     );
